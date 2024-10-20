9809fc3e2de9fb2e1e6c0eac6a10070491190213
dhorvatek21 <domagoj.horvatek2@gmail.com>
2024-10-20 15:14:36 +0200

    vjezba1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <title>Da Vincijev kod</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Da Vincijev kod">
+    <meta name="keywords" content="Da Vinci, kod, triler, knjiga, Dan Brown">
+</head>
+<body>
+    <h1>Da Vincijev kod</h1>
+    <p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.</p>
+    <p>
+        <a href="https://hr.wikipedia.org/wiki/Da_Vincijev_kod" target="_blank">
+            Poveznica.. 
+        </a>
+    </p>
+    
+    <?php
+    // vjezba1
+    ?>
+    
+</body>
+</html>
